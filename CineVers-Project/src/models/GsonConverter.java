ae23674024b6565481469a58778b9662827bac20
AlexisTrey <yulian.tobar13@uptc.edu.co>
2025-09-11 21:30:59 -0500

    Persistencia y CRUD en JSON

--- CineVers-Project/src/models/GsonConverter.java
+++ CineVers-Project/src/models/GsonConverter.java
@@ -4,6 +4,12 @@
  */
 package models;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
  *
  * @author Yulian Alexis Tobar Rios
@@ -12,5 +18,21 @@ package models;
  * @author Jhonnyd Bleyck Arias Santafe
  */
 public class GsonConverter {
-    
+    private final Gson gson;
+
+    public GsonConverter() {
+        this.gson = new GsonBuilder().setPrettyPrinting().create();
+    }
+
+    public void writeObjectToJson(Object object, String path) throws IOException {
+        try (FileWriter writer = new FileWriter(path)) {
+            gson.toJson(object, writer);
+        }
+    }
+
+    public <T> T readObjectFromJson(String path, Class<T> clazz) throws IOException {
+        try (FileReader reader = new FileReader(path)) {
+            return gson.fromJson(reader, clazz);
+        }
+    }
 }
