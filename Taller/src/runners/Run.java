e34ea2626ab12b8d7f07a14656f42ea3b3ba181c
AlexisTrey <yulian.tobar13@uptc.edu.co>
2025-09-28 09:24:13 -0500

    Estructura Base

---
+++ Taller/src/runners/Run.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package runners;
+
+/**
+ *
+ * @author Alexis Tobar R
+ */
+public class Run {
+    
+}
