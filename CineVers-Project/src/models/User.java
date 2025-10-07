5265777726516d4268d3b6fc74d3b0b94d0921f5
AlexisTrey <yulian.tobar13@uptc.edu.co>
2025-10-07 13:39:43 -0500

    Corrección Clase Usuarios

--- CineVers-Project/src/models/User.java
+++ CineVers-Project/src/models/User.java
@@ -12,6 +12,7 @@ package models;
  * @author Jhonnyd Bleyck Arias Santafe
  */
 public class User {
+
     private String id;
     private String firstName;
     private String lastName;
@@ -20,8 +21,11 @@ public class User {
     private String email;
     private String phone;
     private String password;
+    private boolean isAdmin;
+    private Role role;
+    private City city;
 
-    public User(String id, String firstName, String lastName, String documentType, String documentNumber, String email, String phone, String password) {
+    public User(String id, String firstName, String lastName, String documentType, String documentNumber, String email, String phone, String password, boolean isAdmin, Role role, City city) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
@@ -30,10 +34,14 @@ public class User {
         this.email = email;
         this.phone = phone;
         this.password = password;
+        this.isAdmin = isAdmin;
+        this.role = role;
+        this.city = city;
     }
+
     public User() {
     }
-    
+
     public String getId() {
         return id;
     }
@@ -41,59 +49,106 @@ public class User {
     public void setId(String id) {
         this.id = id;
     }
+
     public String getFirstName() {
         return firstName;
     }
+
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
+
     public String getLastName() {
         return lastName;
     }
+
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
+
     public String getDocumentType() {
         return documentType;
     }
+
     public void setDocumentType(String documentType) {
         this.documentType = documentType;
     }
+
     public String getDocumentNumber() {
         return documentNumber;
     }
+
     public void setDocumentNumber(String documentNumber) {
         this.documentNumber = documentNumber;
     }
+
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
+
     public String getPhone() {
         return phone;
     }
+
     public void setPhone(String phone) {
         this.phone = phone;
     }
+
     public String getPassword() {
         return password;
     }
+
     public void setPassword(String password) {
         this.password = password;
     }
-    
+
     public String getFullName() {
         return firstName + " " + lastName;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(boolean isAdmin) {
+        this.isAdmin = isAdmin;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public City getCity() {
+        return city;
+    }
+
+    public void setCity(City city) {
+        this.city = city;
     }
 
     public boolean login(String email, String password) {
         return this.email.equals(email) && this.password.equals(password);
     }
-    
+
     public void changePassword(String newPassword) {
         this.password = newPassword;
+    }
+
+    public boolean canPerformAction(String action) {
+        if (isAdmin) {
+            return true;
+        } else {
+            System.out.println("No tienes permiso para realizar la acción: " + action);
+            return false;
+        }
     }
 
     @Override
