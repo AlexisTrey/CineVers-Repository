0ea4a5e79b05f713d1644e0054efb3668a41b290
AlexisTrey <yulian.tobar13@uptc.edu.co>
2025-10-09 18:39:24 -0500

    MainPanel

--- CineVers-Project/src/views/MainPanel.java
+++ CineVers-Project/src/views/MainPanel.java
@@ -1,6 +1,7 @@
 package views;
 
 import java.awt.BorderLayout;
+import java.awt.CardLayout;
 import java.awt.event.ActionListener;
 
 import javax.swing.JPanel;
@@ -15,13 +16,29 @@ import javax.swing.JPanel;
 public class MainPanel extends JPanel {
 
     private Header header;
+    private JPanel contentPanel;
+    private CardLayout cardLayout;
     private Background background;
+
+    public static final String HOME = "home";
 
     public MainPanel(ActionListener listener) {
         setLayout(new BorderLayout());
         header = new Header(listener);
-        background = new Background();
+        //background = new Background();
+
+        cardLayout = new CardLayout();
+        contentPanel = new JPanel(cardLayout);
+
+        // Agregación de vistas al CardLayout
+        contentPanel.add(new HomePanel(), HOME);
+
         add(header, BorderLayout.NORTH);
-        add(background, BorderLayout.CENTER);
+        add(contentPanel, BorderLayout.CENTER);
+        //add(background, BorderLayout.CENTER);
+    }
+
+    public void showPanel(String panelName) {
+        cardLayout.show(contentPanel, panelName);
     }
 }
