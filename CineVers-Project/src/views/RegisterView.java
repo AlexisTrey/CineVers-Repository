51dede9c4db261d2e4543343348448a584a7baf9
hellen-meloc <hellen.melo@uptc.edu.co>
2025-11-08 21:22:43 -0500

    Update RegisterView.java

--- CineVers-Project/src/views/RegisterView.java
+++ CineVers-Project/src/views/RegisterView.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -337,3 +337,4 @@ public class RegisterView extends JPanel {
     }
 
 }
+
